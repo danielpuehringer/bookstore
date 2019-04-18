f8af8357508bdaaa8ba221fdb6feeada539ac7f5
Daniel Puehringer <danielpuehringer11@gmail.com>
2019-04-18 08:49:48 +0200

    improved seeder with admin user

--- lara-bookstore19/database/seeds/UsersTableSeeder.php
+++ lara-bookstore19/database/seeds/UsersTableSeeder.php
@@ -11,15 +11,26 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //test user
-        $user = new \App\User;
-        $user->name = "testuser";
-        $user->email = 'test@gmail.com';
-        $user->password = bcrypt('secret');//brypt is default encryption
-        $user->isAdmin = false;
-        $user->firstName = 'Maxi';
-        $user->lastName = 'Mustermann';
-        $user->address = 'Fakestreet 106/Top13';
-        $user->save();
+        //test user1
+        $user1 = new \App\User;
+        $user1->name = "testuser";
+        $user1->email = 'test@gmail.com';
+        $user1->password = bcrypt('secret');//brypt is default encryption
+        $user1->isAdmin = false;
+        $user1->firstName = 'Maxi';
+        $user1->lastName = 'Mustermann';
+        $user1->address = 'Fakestreet 106/Top13';
+        $user1->save();
+
+        //test user2
+        $user2 = new \App\User;
+        $user2->name = "admin";
+        $user2->email = 'admin@gmail.com';
+        $user2->password = bcrypt('admin');//brypt is default encryption
+        $user2->isAdmin = true;
+        $user2->firstName = 'John';
+        $user2->lastName = 'Admin';
+        $user2->address = 'Hackerway 106/Top13';
+        $user2->save();
     }
 }
