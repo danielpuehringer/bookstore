1fa54a9a635c313f77dbd7202398cf0d187606e8
Daniel Puehringer <danielpuehringer11@gmail.com>
2019-04-30 11:08:54 +0200

    started with implementing Create-request for order

--- lara-bookstore19/app/Http/Controllers/OrderController.php
+++ lara-bookstore19/app/Http/Controllers/OrderController.php
@@ -33,4 +33,38 @@ class OrderController extends Controller
 
 
     //POST: orderController@save
+    public function save(Request $request) : JsonResponse {
+        $request = $this->parseRequest($request);
+        DB::beginTransaction();
+        try{
+            $order = Order::create($request->all());
+
+            //arrays welche im with übergeben wurden alle auflisten
+            //states
+            //books
+            //TODO
+
+            DB::commit();
+            // return a vaild http response
+            return response()->json($order, 201);
+        }
+        catch(\Exception $e){
+            DB::rollback();
+            return response()->json("saving order failed: " . $e->getMessage(), 420);
+        }
+
+
+
+        return $request;
+    }
+
+    /**
+     * modify / convert values if needed
+     */
+    private function parseRequest(Request $request) : Request {
+        // get date and convert it - its in ISO 8601, e.g. "2018-01-01T23:00:00.000Z"
+        $date = new \DateTime($request->published);
+        $request['published'] = $date;
+        return $request;
+    }
 }
