20b3949ed79e30547cb262eda3de226964ef1ed4
Daniel <danielpuehringer11@gmail.com>
2019-05-01 18:01:55 +0200

    added all orders on home for admin

--- ng-bookstore19/src/app/order-list/order-list.component.ts
+++ ng-bookstore19/src/app/order-list/order-list.component.ts
@@ -10,12 +10,43 @@ import {AuthService} from "../shared/authentication.service";
 })
 export class OrderListComponent implements OnInit {
 
-  public orders: Order[];
+  public orders: Order[] = new Array();
+  public ordersOfUser: Order[] = new Array();
 
   constructor(private os: OrderService, private auth: AuthService) { }
 
   ngOnInit() {
     this.os.getAllOrders().subscribe(res => this.orders = res);
+    if(this.auth.getCurrentUserId()){
+        this.syncOrdersOfUser(this.auth.getCurrentUserId());
+    }
+    this.os.getOrdersOfUser(1);
   }
+
+  //TODO
+  checkIfOrdersExists(): boolean{
+      return this.orders.length > 0;
+  }
+
+  isLoggedIn(): boolean {
+      return this.auth.isLoggedIn();
+  }
+
+  isAdmin(): boolean {
+      let isAdmin : boolean = this.auth.isAdmin();
+      return isAdmin;
+  }
+
+  syncOrdersOfUser(id: number): void {
+      let ordersOfUser = new Array();
+      for(let order of this.orders){
+          if(order.user_id == id){
+              ordersOfUser.push(order);
+          }
+      }
+      this.ordersOfUser = ordersOfUser;
+  }
+
+
 
 }
--- ng-bookstore19/src/app/shared/order.service.ts
+++ ng-bookstore19/src/app/shared/order.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {Observable, throwError} from "rxjs";
-import {catchError, retry} from "rxjs/internal/operators";
+import {Observable, throwError, of} from "rxjs";
+import { ajax } from 'rxjs/ajax';
+import {catchError, retry, map} from "rxjs/internal/operators";
 import {Order} from "./order";
 
 @Injectable({
@@ -10,14 +11,17 @@ import {Order} from "./order";
 export class OrderService {
 
   constructor(private http: HttpClient) { }
-  //private orders: Order[] = new Array();
-  //private ordersOfUser: Order[] = new Array();
 
   private api= "http://bookstore19.s1610456027.student.kwmhgb.at/api";
 
   getAllOrders(): Observable<Array<Order>> {
     return this.http.get(`${this.api}/orders`)
         .pipe(retry(3)).pipe(catchError(this.errorHandler));
+  }
+
+  getOrdersOfUser(user_id: number): Observable<Array<Order>> {
+      return this.http.get(`${this.api}/orders/user/${user_id}`)
+          .pipe(retry(3)).pipe(catchError(this.errorHandler));
   }
 
 
